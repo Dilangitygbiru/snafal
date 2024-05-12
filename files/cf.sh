96b4b01bd3c092c104ac8f9d985039b4e4f6b139
Snafal <153316573+Dilangitygbiru@users.noreply.github.com>
2024-05-12 19:03:20 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -12,11 +12,7 @@ apt install jq curl -y
 rm -rf /root/xray/scdomain
 mkdir -p /root/xray
 clear
-echo ""
-echo ""
-echo ""
-#sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
-read -rp "Input Domain Name. Example ( kontol12 ): " -e sub
+read -rp "Input Domain Name. Example ( sgdo ): " -e sub
 DOMAIN=suprime.my.id
 SUB_DOMAIN=${sub}.suprime.my.id
 CF_ID=prantasoiyoiiy@gmail.com
@@ -45,11 +41,10 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
 -H "Content-Type: application/json" \
 --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 
-echo "$SUB_DOMAIN" > /root/domain
-echo "$SUB_DOMAIN" > /root/scdomain
-echo "$SUB_DOMAIN" > /etc/xray/domain
-echo "$SUB_DOMAIN" > /etc/v2ray/domain
-echo "$SUB_DOMAIN" > /etc/xray/scdomain
 echo "IP=$SUB_DOMAIN" > /var/lib/kyt/ipvps.conf
+echo "$SUB_DOMAIN" > /root/scdomain 
+echo "$SUB_DOMAIN" > /etc/xray/scdomain 
+echo "$SUB_DOMAIN" > /etc/xray/domain 
+echo "$SUB_DOMAIN" > /root/domain 
 rm -rf cf
 sleep 1
